498d8fc90b684a9e53394c251e3fc1b1d7218367
Biswajitdash-09 <biswajitdash929@gmail.com>
2026-02-06 11:52:43 +0530

    aaa

--- app/api/version/route.js
+++ app/api/version/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
-const APP_VERSION = process.env.NEXT_PUBLIC_APP_VERSION || '1.0.9';
+const APP_VERSION = process.env.NEXT_PUBLIC_APP_VERSION || '1.0.10';
 
 export async function GET() {
     return NextResponse.json({
